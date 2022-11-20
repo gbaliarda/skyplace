0bdc2e6138031b17c83599dd034042b7bb2a3811
VALENTIN YE LI <vyeli@itba.edu.ar>
2022-11-20 18:23:08 -0300

    feat: get user inventory

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserController.java
@@ -1,12 +1,14 @@
 package ar.edu.itba.paw.webapp.controller;
 
-import ar.edu.itba.paw.model.Nft;
+
 import ar.edu.itba.paw.model.Publication;
 import ar.edu.itba.paw.model.Review;
 import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.service.BuyOrderService;
 import ar.edu.itba.paw.service.NftService;
 import ar.edu.itba.paw.service.ReviewService;
 import ar.edu.itba.paw.service.UserService;
+import ar.edu.itba.paw.webapp.dto.BuyOrderDto;
 import ar.edu.itba.paw.webapp.dto.NftDto;
 import ar.edu.itba.paw.webapp.dto.ReviewDto;
 import ar.edu.itba.paw.webapp.dto.UserDto;
@@ -29,14 +31,19 @@ public class UserController {
 
     private final UserService userService;
     private final ReviewService reviewService;
+    private final BuyOrderService buyOrderService;
+
+    private final NftService nftService;
 
     @Context
     private UriInfo uriInfo;
 
     @Autowired
-    public UserController(UserService userService, ReviewService reviewService) {
+    public UserController(UserService userService, ReviewService reviewService, BuyOrderService buyOrderService, NftService nftService) {
         this.userService = userService;
         this.reviewService = reviewService;
+        this.buyOrderService = buyOrderService;
+        this.nftService = nftService;
     }
 
     @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED, })
@@ -100,6 +107,69 @@ public class UserController {
                 .link(uriInfo.getAbsolutePathBuilder().queryParam("page", 1).build(), "first")
                 .link(uriInfo.getAbsolutePathBuilder().queryParam("page", lastPage).build(), "last")
                 .build();
+    }
+
+    @GET
+    @Path("/{id}/buyorders")
+    @Produces({ MediaType.APPLICATION_JSON, })
+    public Response getBuyOrdersByUserId(@PathParam("id") final int userId, @QueryParam("page") @DefaultValue("1") final int page, @QueryParam("status") @DefaultValue("ALL") final String status) {
+        Optional<User> maybeUser = userService.getUserById(userId);
+        if (!maybeUser.isPresent()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        long amountOfferPages;
+        amountOfferPages = buyOrderService.getAmountPagesForUser(maybeUser.get());
+
+        if(page > amountOfferPages || page < 0 ) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        List<BuyOrderDto> buyOffers;
+        buyOffers = buyOrderService.getBuyOrdersForUser(maybeUser.get(), page, status).stream().map(n -> BuyOrderDto.fromBuyOrder(n, uriInfo)).collect(Collectors.toList());
+
+        if(buyOffers.isEmpty()){
+            return Response.noContent().build();
+        }
+        Response.ResponseBuilder responseBuilder = Response.ok(new GenericEntity<List<BuyOrderDto>>(buyOffers) {});
+        if (page > 1)
+            responseBuilder.link(uriInfo.getAbsolutePathBuilder().queryParam("page", page - 1).build(), "prev");
+        if (page < amountOfferPages)
+            responseBuilder.link(uriInfo.getAbsolutePathBuilder().queryParam("page", page + 1).build(), "next");
+        return responseBuilder
+                .link(uriInfo.getAbsolutePathBuilder().queryParam("page", 1).build(), "first")
+                .link(uriInfo.getAbsolutePathBuilder().queryParam("page", amountOfferPages).build(), "last")
+                .build();
+    }
+
+
+    @GET
+    @Path("/{id}/inventory")
+    @Produces({ MediaType.APPLICATION_JSON, })
+    public Response getUserInventoryByUserId(@PathParam("id") final int userId, @QueryParam("page") @DefaultValue("1") final int page, @QueryParam("sort") final String sort) {
+        Optional<User> maybeUser = userService.getUserById(userId);
+
+        if(!maybeUser.isPresent()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        List<NftDto> userInventory = nftService.getAllPublicationsByUser(page, maybeUser.get(), "inventory", sort)
+                .stream().map(Publication::getNft).map(n -> NftDto.fromNft(uriInfo, n)).collect(Collectors.toList());
+        if (userInventory.isEmpty())
+            return Response.noContent().build();
+
+        Response.ResponseBuilder responseBuilder = Response.ok(new GenericEntity<List<NftDto>>(userInventory) {});
+        if (page > 1)
+            responseBuilder.link(uriInfo.getAbsolutePathBuilder().queryParam("page", page - 1).build(), "prev");
+        int lastPage = nftService.getAmountPublicationPagesByUser(maybeUser.get(), maybeUser.get(), "inventory");
+        if (page < lastPage)
+            responseBuilder.link(uriInfo.getAbsolutePathBuilder().queryParam("page", page + 1).build(), "next");
+
+        return responseBuilder
+                .link(uriInfo.getAbsolutePathBuilder().queryParam("page", 1).build(), "first")
+                .link(uriInfo.getAbsolutePathBuilder().queryParam("page", lastPage).build(), "last")
+                .build();
+
     }
 
 }
