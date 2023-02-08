bbfa9900ff031f9d62a416c7ab6a66a15d9945d4
gbaliarda <gbaliarda@itba.edu.ar>
2023-02-08 15:56:48 -0300

    fix: lint errors

--- spa/pages/login.tsx
+++ spa/pages/login.tsx
@@ -120,6 +120,7 @@ const LoginInput = ({ type, placeholder, value, onChange }: LoginInputProps) => 
       required
       className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 focus:z-10 sm:text-sm"
       placeholder={placeholder}
+      suppressHydrationWarning
     />
   </div>
 )
