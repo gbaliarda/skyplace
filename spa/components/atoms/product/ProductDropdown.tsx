4051c38e5703caf8ed97f39850d3378963f7014a
Ezequiel <eperez@itba.edu.ar>
2023-02-09 15:08:06 -0300

    feat: update/delete sell orders, delete nfts

--- spa/components/atoms/product/ProductDropdown.tsx
+++ spa/components/atoms/product/ProductDropdown.tsx
@@ -1,13 +1,80 @@
 import { EllipsisVerticalIcon } from "@heroicons/react/24/outline"
 import { useTranslation } from "next-export-i18n"
 import Link from "next/link"
+import { useRouter } from "next/router";
+import Nft from "../../../types/Nft"
+import { useSellorderUrl } from '../../../services/sellorders';
+import Swal from "sweetalert2";
+import { fetcher } from "../../../services/endpoints";
+import useSession from '../../../hooks/useSession';
+import { useNft } from '../../../services/nfts';
+import { useUserUrl } from '../../../services/users';
 
 interface Props {
-  isOnSale: boolean
+  nft: Nft
 }
 
-const ProductDropdown = ({ isOnSale }: Props) => {
+const ProductDropdown = ({ nft }: Props) => {
   const { t } = useTranslation()
+  const router = useRouter()
+  const { accessToken } = useSession()
+  const { mutate } = useNft(nft.id)
+  const { sellorder } = useSellorderUrl(nft.sellorder?.toString())
+  const { user } = useUserUrl(nft.owner.toString())
+
+  const isOnSale = nft.sellorder !== undefined
+
+  const handleDeleteSellorder = async () => {
+    try {
+      const { isConfirmed } = await Swal.fire({
+        title: t("product.deleteSellOrderConfirm"),
+        text: t("product.deleteSellOrderInfo"),
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: t("product.removeOffer"),
+        cancelButtonText: t("product.cancel")
+      })
+      if (isConfirmed) {
+        await fetcher(`/sellorders/${sellorder!!.id}`, {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        Swal.fire({ title: t("product.deleteSellOrderSuccess"), icon: "success" })
+        mutate()
+      }
+    } catch(e: any) {
+      console.log(e.name, e.message)
+      Swal.fire({ title: t("product.deleteSellOrderError"), text: e.message, icon: "error" })
+    }
+  }
+
+  const handleDeleteNft = async () => {
+    try {
+      const { isConfirmed } = await Swal.fire({
+        title: t("product.deleteNftConfirm"),
+        text: t("product.deleteNftInfo"),
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: t("product.removeOffer"),
+        cancelButtonText: t("product.cancel")
+      })
+      if (isConfirmed) {
+        await fetcher(`/nfts/${nft.id}`, {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        await Swal.fire({ title: t("product.deleteNftSuccess"), icon: "success" })
+        router.replace(`/profile/${user!!.id}`)
+      }
+    } catch(e: any) {
+      console.log(e.name, e.message)
+      Swal.fire({ title: t("product.deleteNftError"), text: e.message, icon: "error" })
+    }
+  }
 
   return (
     <div className="dropdown">
@@ -23,24 +90,23 @@ const ProductDropdown = ({ isOnSale }: Props) => {
           className="dropdown-content menu mt-2 border border-gray-300 text-sm shadow bg-base-100 rounded-md"
         >
           <li>
-            <Link href={""}>
+            <Link href={`/sell/${nft.id}?update=${sellorder?.id}&category=${sellorder?.category}&price=${sellorder?.price}`}>
               <span
                 suppressHydrationWarning
-                className="text-xs whitespace-nowrap hover:bg-gray-600 rounded-t-md hover:text-white transition-none py-1.5"
+                className="text-xs whitespace-nowrap hover:bg-gray-600 rounded-t-md hover:text-white transition-none py-2"
               >
                 {t("product.updateSell")}
               </span>
             </Link>
           </li>
           <li>
-            <Link href={""}>
-              <span
-                suppressHydrationWarning
-                className="text-xs whitespace-nowrap hover:bg-gray-600 rounded-b-md hover:text-white transition-none py-2 pr-12"
-              >
-                {t("product.deleteSell")}
-              </span>
-            </Link>
+            <button
+              onClick={handleDeleteSellorder}
+              suppressHydrationWarning
+              className="text-xs whitespace-nowrap hover:bg-gray-600 rounded-b-md hover:text-white transition-none py-2 pr-12"
+            >
+              {t("product.deleteSell")}
+            </button>
           </li>
         </ul>
       ) : (
@@ -49,24 +115,23 @@ const ProductDropdown = ({ isOnSale }: Props) => {
           className="dropdown-content menu mt-2 border border-gray-300 text-sm shadow bg-base-100 rounded-md"
         >
           <li>
-            <Link href={""}>
+            <Link href={`/sell/${nft.id}`}>
               <span
                 suppressHydrationWarning
-                className="text-xs whitespace-nowrap hover:bg-gray-600 rounded-t-md hover:text-white transition-none py-1.5"
+                className="text-xs whitespace-nowrap hover:bg-gray-600 rounded-t-md hover:text-white transition-none py-2"
               >
                 {t("product.sell")}
               </span>
             </Link>
           </li>
           <li>
-            <Link href={""}>
-              <span
-                suppressHydrationWarning
-                className="text-xs whitespace-nowrap hover:bg-gray-600 rounded-b-md hover:text-white transition-none py-2 pr-12"
-              >
-                {t("product.delete")}
-              </span>
-            </Link>
+            <button
+              onClick={handleDeleteNft}
+              suppressHydrationWarning
+              className="text-xs whitespace-nowrap hover:bg-gray-600 rounded-b-md hover:text-white transition-none py-2 pr-12"
+            >
+              {t("product.delete")}
+            </button>
           </li>
         </ul>
       )}
--- spa/pages/sell/[id].tsx
+++ spa/pages/sell/[id].tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router"
 import Error from "next/error"
-import { FormEvent, useState } from "react"
+import { FormEvent, useEffect, useState } from "react"
 import Swal from "sweetalert2"
 import { useTranslation } from "next-export-i18n"
 import { useNft } from "../../services/nfts"
@@ -10,7 +10,7 @@ import FormSelect from "../../components/atoms/forms/FormSelect"
 import FormNumber from "../../components/atoms/forms/FormNumber"
 import FormSubmit from "../../components/atoms/forms/FormSubmit"
 import Navbar from "../../components/molecules/Navbar"
-import { postJson } from "../../services/endpoints"
+import { sendJson } from "../../services/endpoints"
 import useSession from "../../hooks/useSession"
 
 const CATEGORIES = [
@@ -38,12 +38,18 @@ const INITIAL_DATA: FormData = {
 
 export default function Sell() {
   const router = useRouter()
+  const { update, category, price } = router.query as { update?: string, category?: string, price?: number }
   const { t } = useTranslation()
   const { id } = router.query as { id: string }
   const { nft, error } = useNft(id)
   const [data, updateFields] = useForm<FormData>(INITIAL_DATA)
   const { accessToken } = useSession()
   const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    if (category) updateFields({ category })
+    if (price) updateFields({ price })
+  }, [category, price])
 
   if (error)
     return (
@@ -59,11 +65,14 @@ export default function Sell() {
 
     setLoading(true)
     try {
-      await postJson("/sellorders", { ...data, nftId: id }, accessToken)
+      const method = update ? "PUT" : "POST"
+      const endpoint = update ? `/sellorders/${update}` : "/sellorders"
+      await sendJson(method, endpoint, { ...data, nftId: id }, accessToken)
       router.push(`/product/${id}`)
     } catch (err: any) {
       console.log(err.name, err.message)
-      Swal.fire({ title: t("errors.sellNft"), text: err.message, icon: "error" })
+      const errorTitle = update? t("errors.updateSellorder") : t("errors.sellNft")
+      Swal.fire({ title: errorTitle, text: err.message, icon: "error" })
     }
     setLoading(false)
   }
@@ -71,31 +80,31 @@ export default function Sell() {
   return (
     <Layout>
       <div className="w-full max-w-5xl mx-auto p-4 grow flex flex-col justify-center py-12">
-        <h1 className="text-3xl text-center">Sell an NFT</h1>
+        <h1 suppressHydrationWarning className="text-3xl text-center">{t("sell.sellNft")}</h1>
 
         <form onSubmit={handleSubmit} className="grid grid-cols-2 items-end gap-8 pt-16">
           <div className="flex flex-col gap-1">
             <p className="text-slate-600">
-              <span className="font-bold">NFT id:</span>
+              <span suppressHydrationWarning className="font-bold">{t("sell.nftId")}: </span>
               {nft?.nftId}
             </p>
           </div>
           <div className="flex flex-col gap-1">
             <p className="text-slate-600">
-              <span className="font-bold">NFT contract:</span>
+              <span suppressHydrationWarning className="font-bold">{t("sell.nftContract")}: </span>
               {nft?.contractAddr}
             </p>
           </div>
 
           <FormSelect
-            name="Category"
+            name={t("sell.category")}
             options={CATEGORIES}
             value={data.category}
             onChange={(e) => updateFields({ category: e.target.value })}
           />
 
           <FormNumber
-            name="Price (ETH)"
+            name={`${t("sell.price")} (ETH)`}
             decimals={18}
             value={data.price}
             onChange={(e) => updateFields({ price: parseFloat(e.target.value) })}
@@ -103,7 +112,7 @@ export default function Sell() {
 
           <FormSubmit
             disabled={loading}
-            value="Publish"
+            value={update ? t("sell.update") : t("sell.publish")}
             classes="px-1 py-4 col-start-2 font-bold rounded-lg shadow-sm cursor-pointer bg-cyan-100 text-cyan-700 hover:bg-cyan-200 disabled:bg-gray-100 disabled:text-gray-500"
           />
         </form>
