40891fb6aeb10c100e0d44c12397bebf006881bf
gbaliarda <gbaliarda@itba.edu.ar>
2023-02-08 22:42:38 -0300

    chore: modularize error page

---
+++ spa/components/molecules/ErrorPage.tsx
@@ -0,0 +1,45 @@
+import { useTranslation } from "next-export-i18n"
+import Link from "next/link"
+import Navbar from "./Navbar"
+
+interface Props {
+  errorCode: number
+  errorTitle: string
+  errorDetail: string
+}
+
+const ErrorPage = ({ errorCode, errorTitle, errorDetail }: Props) => {
+  const { t } = useTranslation()
+
+  return (
+    <div className="mx-5">
+      <div className="h-screen flex flex-col">
+        <Navbar />
+        <div className="flex flex-col sm:flex-row justify-center items-center flex-grow sm:divide-x h-">
+          <h1 className="text-8xl text-cyan-500 pr-5 text-bold">{errorCode}</h1>
+          <div className="flex flex-col justify-start pl-5 gap-y-1">
+            <h2 suppressHydrationWarning className="text-5xl text-bold font-bold">
+              {errorTitle}
+            </h2>
+            <p suppressHydrationWarning className="text-xl text-bold font-light">
+              {errorDetail}
+            </p>
+            <Link href="/">
+              <a>
+                <button
+                  suppressHydrationWarning
+                  type="button"
+                  className="w-fit shadow-md mt-3 py-2 px-6 rounded-md transition duration-300 bg-cyan-600 text-white"
+                >
+                  {t("errors.goBack")}
+                </button>
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ErrorPage
--- spa/pages/403.tsx
+++ spa/pages/403.tsx
@@ -1,37 +1,13 @@
 import { useTranslation } from "next-export-i18n"
-import Link from "next/link"
-import Navbar from "../components/molecules/Navbar"
+import ErrorPage from "../components/molecules/ErrorPage"
 
 export default function Custom403() {
   const { t } = useTranslation()
-
   return (
-    <div className="mx-5">
-      <div className="h-screen flex flex-col">
-        <Navbar />
-        <div className="flex flex-col sm:flex-row justify-center items-center flex-grow sm:divide-x h-">
-          <h1 className="text-8xl text-cyan-500 pr-5 text-bold">403</h1>
-          <div className="flex flex-col justify-start pl-5 gap-y-1">
-            <h2 suppressHydrationWarning className="text-5xl text-bold font-bold">
-              {t("403.forbidden")}
-            </h2>
-            <p suppressHydrationWarning className="text-xl text-bold font-light">
-              {t("403.noPermission")}
-            </p>
-            <Link href="/">
-              <a>
-                <button
-                  suppressHydrationWarning
-                  type="button"
-                  className="w-fit shadow-md mt-3 py-2 px-6 rounded-md transition duration-300 bg-cyan-600 text-white"
-                >
-                  {t("403.goBack")}
-                </button>
-              </a>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
+    <ErrorPage
+      errorCode={403}
+      errorTitle={t("403.forbidden")}
+      errorDetail={t("403.noPermission")}
+    />
   )
 }
